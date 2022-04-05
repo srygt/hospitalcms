4978add62f97f2cda8f58d4aec2e84110d74af9b
Serdar Yigit <53877866+srygt@users.noreply.github.com>
2022-04-05 22:24:30 +0300

    Delete resources/views/yonetici directory

--- resources/views/yonetici/layout/admin/partials/css.blade.php
+++
@@ -1,5 +0,0 @@
-<link rel="icon" type="image/png" sizes="16x16" href="{{asset('/yonetici/images/favicon.png')}}">
-<link href="{{asset('yonetici/vendor/owl-carousel/owl.carousel.css')}}" rel="stylesheet">
-<link href="{{asset('yonetici/vendor/bootstrap-select/dist/css/bootstrap-select.min.css')}}" rel="stylesheet">
-<link href="{{asset('yonetici/css/style.css')}}" rel="stylesheet">
-<link href="https://cdn.lineicons.com/2.0/LineIcons.css" rel="stylesheet">
